289d8dad812e5b7d7889bb3fc28da615a8c0790f
Ted Williams <tboxyz@gmail.com>
2019-08-24 15:17:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [one,two,three,four]
+  my_array=[one,two,three,four]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,17 +23,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-array[index_number]
+array[index_number]= element
 end
